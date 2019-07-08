724800db13e7c1847eebaf96f86172200f248e3e
Omniscimus <reinier@reinier.info>
2019-07-08 22:18:09 +0200

    Add soil

--- colony.js
+++ colony.js
@@ -5,6 +5,9 @@ var config = {
     type: Phaser.AUTO,
     width: width,
     height: height,
+    physics: {
+        default: 'arcade'
+    },
     scene: {
         preload: preload,
         create: create
@@ -15,8 +18,14 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('soil', 'assets/soil.png');
 }
 
 function create ()
 {
+    soilGroup = this.physics.add.staticGroup();
+    soil = soilGroup.create(width, height, 'soil');
+    soil.displayWidth = width;
+    soil.displayHeight = height / 2;
+    soil.refreshBody();
 }
