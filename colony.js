7affc0a107f344621c86e02851f2126ac7a4048a
Omniscimus <reinier@reinier.info>
2019-07-09 10:23:14 +0200

    Add an ant

--- colony.js
+++ colony.js
@@ -10,7 +10,8 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
@@ -19,6 +20,7 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('soil', 'assets/soil.png');
+    this.load.spritesheet('ant', 'assets/ant.png', {frameWidth: 32, frameHeight: 23});
 }
 
 function create ()
@@ -28,4 +30,39 @@ function create ()
     soil.displayWidth = width;
     soil.displayHeight = height / 2;
     soil.refreshBody();
+
+    ant = this.physics.add.sprite(width / 2, height / 2, 'ant');
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('ant', {start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'stationary',
+        frames: [{key: 'ant', frame: 4}],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('ant', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update () {
+    if (cursors.left.isDown) {
+        ant.setVelocityX(-100);
+        ant.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        ant.setVelocityX(100);
+        ant.anims.play('right', true);
+    } else {
+        ant.setVelocityX(0);
+        ant.anims.play('stationary');
+    }
 }
